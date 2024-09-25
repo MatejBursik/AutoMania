ad6a7a461ef3cedff360728cc89eee767d3f364f
MatejBursik <r0929146@student.thomasmore.be>
2024-09-25 13:27:09 +0200

    prepared the app loop with necessery function

---
+++ Supervised/functions.py
@@ -0,0 +1,71 @@
+import cv2, numpy as np, pyautogui as pg, keyboard, pygetwindow as gw
+
+def process_result(result):
+    """
+    - result is the answere of the neural network where 0 = up, 1 = down, 2 = left, 3 = right
+    - create keyboard input based on the result
+    """
+
+    for i,v in enumerate(result):
+        if v > 0.7:
+            match i:
+                case 0:
+                    keyboard.press("up")
+                case 1:
+                    keyboard.press("down")
+                case 2:
+                    keyboard.press("left")
+                case 3:
+                    keyboard.press("right")
+        else:
+            match i:
+                case 0:
+                    keyboard.release("up")
+                case 1:
+                    keyboard.release("down")
+                case 2:
+                    keyboard.release("left")
+                case 3:
+                    keyboard.release("right")
+
+
+def get_window_info(title):
+    window = gw.getWindowsWithTitle(title)
+
+    if window:
+        # get the first matching window
+        app_window = window[0]
+        
+        # get window position and resolution
+        pos = (app_window.left, app_window.top)
+        res = (app_window.width, app_window.height)
+        return (pos, res)
+
+    return None
+
+
+def take_a_screenshot(title):
+    """
+    - take a screenshot of the window with the specified title
+    - return a numpy array
+    """
+    win_pos, res = get_window_info(title)
+    img = pg.screenshot(region=(win_pos[0], win_pos[1], res[0], res[1]))
+
+    # process the screenshot
+    screen = np.array(img)
+    screen = cv2.resize(screen, (0, 0), fx = 0.5, fy = 0.5) # down scale by half
+    screen = screen[17:-5, 5:-5] # removing unwanted edges
+    screen = cv2.cvtColor(screen, cv2.COLOR_RGB2BGR) # color correct
+    
+    return screen
+
+
+def decision(screen):
+    """
+    - apply the neural network model onto the screenshot
+    - get a decision out of it and store it in result
+    """
+    result = None
+
+    return result
---
+++ Supervised/main.py
@@ -0,0 +1,16 @@
+from functions import *
+
+title = "Trackmania"
+run = False
+
+while not run:
+    if keyboard.is_pressed('shift'): # start recording by 'shift'
+        run = True
+        print("Recording started ...")
+
+while run:
+    if keyboard.is_pressed('ctrl'): # end recording by 'ctrl'
+        break
+    screen = take_a_screenshot(title)
+    result = decision(screen)
+    process_result(result)
